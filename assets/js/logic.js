cd2f73473ff2cb07832ee25d241d6bf3b6f26b5d
ParboldCoder <darren.a.buck@gmail.com>
2024-01-11 18:36:49 +0000

    started logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const startScreen = document.getElementById("start-screen");
+  const quizContainer = document.getElementById("questions");
+  const questionTitle = document.getElementById("question-title");
+  const choicesContainer = document.getElementById("choices");
+  const endScreen = document.getElementById("end-screen");
+  const finalScore = document.getElementById("final-score");
+  const initialsInput = document.getElementById("initials");
+  const submitButton = document.getElementById("submit");
+  const timerDisplay = document.getElementById("time");
+  const startButton = document.getElementById("start");
+  const feedbackContainer = document.getElementById("feedback");
+  const scoresLink = document.querySelector(".scores a");
+
+}
