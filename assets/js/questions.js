9d5a1e8909825a2d957b11cdd2c4b46c802e7c92
ParboldCoder <darren.a.buck@gmail.com>
2024-01-12 15:20:01 +0000

    added questions to the project

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    question: "In 1983, this groundbreaking arcade game featured had the main protaganist who could rotate the world around him. What is the title of this innovative game?",
+    choices: ["Joust", "Gyruss", "Black Tiger", "Knight Lore"],
+    correctAnswer: "Knight Lore"
+  },
+  {
+    question: "Released in 1984, this platformer introduced a character named Harry, navigating through a jungle filled with hazards. What is the name of the game?",
+    choices: ["Montezuma's Revenge", "Spelunker", "Pitfall II: Lost Caverns", "Aztec Challenge"],
+    correctAnswer: "Pitfall II: Lost Caverns"
+  },
+  {
+    question: "Which 1985 shoot 'em up game, known for its unique control scheme and cinematic presentation, features a spacecraft named R-9a Arrowhead?",
+    choices: ["Gradius", "Xevious", "R-Type", "Galaga '88"],
+    correctAnswer: "R-Type"
+  },
+  {
+    question: "In 1982, this action-adventure game featured a protagonist named Quink, who explores an open world filled with puzzles and monsters. What is the game's title?",
+    choices: ["E.T. the Extra-Terrestrial", "Faxanadu", "Dragon Slayer", "Hydlide"],
+    correctAnswer: "Hydlide"
+  },
+  {
+    question: "Which 1989 puzzle-platformer, known for its surreal atmosphere and mind-bending gameplay, requires players to manipulate gravity? What is its name?",
+    choices: ["Marble Madness", "Zynaps", "Solstice", "Mind Walker"],
+    correctAnswer: "Solstice"
+  }
+];
